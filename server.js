3023513dfd8d96846cb28ee6e7c9b6c6a9558124
Gabriel Cesario <cesario203@outlook.com>
2020-09-10 15:53:33 -0300

    Criando rota de criação de mensagens, adicionando conexão com o mongo, instalando pusher

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 // Importações e etc
 import express from 'express'
 import mongoose from 'mongoose'
+import Messages from './dbMessages.js'
+import Pusher from 'pusher'
 
 //Configurações do backend
 const app = express()
 const port = process.env.PORT || 8080
 const connection_url = "mongodb+srv://admin:kPPVw79sqhsjkVsA@cluster0.yjfqt.gcp.mongodb.net/zapdb?retryWrites=true&w=majority"
 
+const pusher = new Pusher({
+    appId: '1070801',
+    key: 'b452de844577660cf20a',
+    secret: '6b9fcbd654fc6d176c92',
+    cluster: 'us2',
+    encrypted: true
+  })
+
 //Middlewares
+app.use(express.json())
 
 //Configurações de banco de dados
 mongoose.connect(connection_url,{
@@ -16,7 +27,27 @@ mongoose.connect(connection_url,{
     useUnifiedTopology: true
 })
 
+const db =  mongoose.connection
+
+db.once('open', () => {
+    console.log('Conexão iniciada com sucesso')
+})
+
 //Rotas da API
 app.get('/', (req,res)=>res.status(200).send('De novo novamente essa palhaçada'))
+
+app.get('/messages/sync', (req,res)=>{
+    Messages.find((error,data)=>{
+        error ? res.status(500).send(error) : res.status(200).send(data)
+    })
+})
+
+app.post('/messages/new', (req,res)=>{
+    const dbMessage = req.body
+
+    Messages.create(dbMessage, (error, data)=>{
+        error ? res.status(500).send(error) : res.status(201).send(data)
+    })
+})
 
 app.listen(port, ()=>console.log('Backend rodando na porta '+ port))
